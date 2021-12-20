40c98d696426620e2bac348db2809996e43340ff
Corbin Crutchley <crutchcorn@gmail.com>
2021-12-20 12:14:35 -0800

    chore: adding eslint + prettier

---
+++ apps/web-comp-example/.eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = require("@seaside/config/eslint-preset");
--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,3 +1,24 @@
 module.exports = {
-  extends: ["react-app", "react-app/jest", "prettier"]
+  plugins: ["prettier"],
+  extends: ["react-app", "react-app/jest", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "testing-library/prefer-screen-queries": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_|h",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["*.stories.tsx"],
+      rules: {
+        "import/no-anonymous-default-export": "off",
+      },
+    },
+  ],
 };
