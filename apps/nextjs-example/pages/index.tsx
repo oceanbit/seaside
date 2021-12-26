c84c9087220263bb3b549a49d2312fbfcd92678c
Corbin Crutchley <crutchcorn@gmail.com>
2021-12-26 08:03:53 -0800

    chore: fix exports for some versions of TypeScript

--- apps/nextjs-example/pages/index.tsx
+++ apps/nextjs-example/pages/index.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from "next";
+import { Switch } from "@seaside/components/react";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [enabled, setEnabled] = useState(false);
   return (
-    <div>
-      <p>Hello!</p>
-    </div>
+    <Switch
+      enabled={enabled}
+      disabled={false}
+      onToggle={() => setEnabled(!enabled)}
+    />
   );
 };
 
