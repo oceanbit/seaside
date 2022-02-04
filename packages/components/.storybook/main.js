9e0d064910caa6b0d2a9273fd55c3e2e2b8ae727
Corbin Crutchley <crutchcorn@gmail.com>
2022-02-04 01:34:04 -0800

    chore: fix deployed storybook icon rendering

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -7,5 +7,37 @@ module.exports = {
     builder: "webpack5",
   },
   typescript: { reactDocgen: false },
-  staticDirs: ['./public']
+  staticDirs: ['./public'],
+  previewHead: head => {
+    if (process.env.CI) {
+      return `
+          <style>
+            @font-face {
+              src: url('/seaside/icomoon.ttf');
+              font-family: "SeasideIcon";
+            }
+          </style>
+          ${head}
+      `;
+    }
+    return `
+      <style>
+        @font-face {
+          src: url('/icomoon.ttf');
+          font-family: "SeasideIcon";
+        }
+      </style>
+      ${head}
+    `;
+  },
+  webpackFinal: async (config, { configType }) => {
+    // GitHub Actions CI
+    if (process.env.CI) config.output.publicPath = '/seaside/';
+    return config;
+  },
+  managerWebpack: async (config) => {
+    // GitHub Actions CI
+    if (process.env.CI) config.output.publicPath = '/seaside/';
+    return config;
+  }
 };
