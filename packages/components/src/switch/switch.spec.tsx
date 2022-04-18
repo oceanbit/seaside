c4e7f354a17ac20b0466d9724d2b303d37b7f38d
Corbin Crutchley <git@crutchcorn.dev>
2022-04-18 03:59:44 -0700

    chore: fix tests associated with switch

--- packages/components/src/switch/switch.spec.tsx
+++ packages/components/src/switch/switch.spec.tsx
@@ -1,57 +1,66 @@
 import { Switch } from "./switch";
 import { render, fireEvent } from "@testing-library/react-native";
-import { useState } from "react";
+import { ComponentProps, ReactNode, useState } from "react";
+import { StorybookProvider } from "../storybook-provider";
 
-test("Should render enabled", () => {
-  const { getByTestId } = render(
-    <Switch enabled={false} onToggle={() => {}} disabled={false} />
-  );
-  expect(getByTestId("switch")).not.toBeDisabled();
-});
+const renderSwitch = (children: ReactNode) => {
+  return render(<StorybookProvider>{children}</StorybookProvider>);
+};
 
-test("Should render disabled", () => {
-  const { getByTestId } = render(
-    <Switch enabled={false} onToggle={() => {}} disabled={true} />
-  );
-  expect(getByTestId("switch")).toBeDisabled();
-});
-
-test("Should toggle when enabled", () => {
-  const DisabledSwitch = () => {
-    const [enabled, setEnabled] = useState(false);
-    return <Switch enabled={enabled} onToggle={setEnabled} disabled={false} />;
-  };
-
-  const { getByTestId } = render(<DisabledSwitch />);
-  const switchInstance = getByTestId("switch");
-  expect(switchInstance).toHaveProp("accessibilityState", {
-    disabled: false,
-    selected: false,
+describe("Switch", function () {
+  test("Should render enabled", () => {
+    const { getByTestId } = renderSwitch(
+      <Switch enabled={false} onToggle={() => {}} disabled={false} />
+    );
+    expect(getByTestId("switch")).not.toBeDisabled();
   });
-  fireEvent(switchInstance, "press");
-  expect(getByTestId("switch")).toHaveProp("accessibilityState", {
-    disabled: false,
-    selected: true,
+
+  test("Should render disabled", () => {
+    const { getByTestId } = renderSwitch(
+      <Switch enabled={false} onToggle={() => {}} disabled={true} />
+    );
+    expect(getByTestId("switch")).toBeDisabled();
   });
+
+  test("Should toggle when enabled", () => {
+    const DisabledSwitch = () => {
+      const [enabled, setEnabled] = useState(false);
+      return (
+        <Switch enabled={enabled} onToggle={setEnabled} disabled={false} />
+      );
+    };
+
+    const { getByTestId } = renderSwitch(<DisabledSwitch />);
+    const switchInstance = getByTestId("switch");
+    expect(switchInstance).toHaveProp("accessibilityState", {
+      disabled: false,
+      selected: false,
+    });
+    fireEvent(switchInstance, "press");
+    expect(getByTestId("switch")).toHaveProp("accessibilityState", {
+      disabled: false,
+      selected: true,
+    });
+  });
+
+  test("Should not toggle when disabled", () => {
+    const DisabledSwitch = () => {
+      const [enabled, setEnabled] = useState(false);
+      return <Switch enabled={enabled} onToggle={setEnabled} disabled={true} />;
+    };
+
+    const { getByTestId } = renderSwitch(<DisabledSwitch />);
+    const switchInstance = getByTestId("switch");
+    expect(switchInstance).toHaveProp("accessibilityState", {
+      disabled: true,
+      selected: false,
+    });
+    fireEvent(switchInstance, "press");
+    expect(switchInstance).toHaveProp("accessibilityState", {
+      disabled: true,
+      selected: false,
+    });
+  });
+
+  test.todo("Should toggle for web components");
 });
-
-test("Should not toggle when disabled", () => {
-  const DisabledSwitch = () => {
-    const [enabled, setEnabled] = useState(false);
-    return <Switch enabled={enabled} onToggle={setEnabled} disabled={true} />;
-  };
-
-  const { getByTestId } = render(<DisabledSwitch />);
-  const switchInstance = getByTestId("switch");
-  expect(switchInstance).toHaveProp("accessibilityState", {
-    disabled: true,
-    selected: false,
-  });
-  fireEvent(switchInstance, "press");
-  expect(switchInstance).toHaveProp("accessibilityState", {
-    disabled: true,
-    selected: false,
-  });
-});
-
-test.todo("Should toggle for web components");
