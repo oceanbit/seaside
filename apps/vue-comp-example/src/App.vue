37d48b91fa162d61efbdad3fae24000a02664848
Corbin Crutchley <git@crutchcorn.dev>
2022-04-23 19:52:47 -0700

    chore: add Vue example

---
+++ apps/vue-comp-example/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <sea-switch :enabled="enabled" @toggle="setEnabled(!enabled)" />
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      enabled: false,
+    };
+  },
+  methods: {
+    setEnabled(val) {
+      this.enabled = val;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ apps/vue-comp-example/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  chainWebpack: (config) => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap((options) => ({
+        ...options,
+        compilerOptions: {
+          isCustomElement: (tag) => tag.includes("-"),
+        },
+      }));
+  },
+};
