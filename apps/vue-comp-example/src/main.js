ee02232ef05c73a4cbb323170c00ec203c07bd8d
Corbin Crutchley <git@crutchcorn.dev>
2022-05-14 16:09:03 -0700

    chore: remove vue and preact examples

--- apps/vue-comp-example/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import { register } from "@seaside/components/dist/compat";
-
-const app = createApp(App);
-app.mount("#app");
-register();
